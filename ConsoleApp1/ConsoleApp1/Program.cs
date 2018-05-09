03999792bf9f94e8cbbfeefe2230ab25350fadc4
Ei Hnin Phyu <eihninphyu1996@gmail.com>
2018-05-09 18:54:43 +0630

    Add

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -12,7 +12,6 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello World");
             Console.WriteLine("Hello World");     
-
                        Console.Read ();
         }
     }
